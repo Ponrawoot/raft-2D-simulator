ce53194268cded98bbb410c1b697cc2e1cfb43c0
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2021-11-12 19:14:03 +0700

    Create class Weapon

--- src/object/Weapon.java
+++ src/object/Weapon.java
@@ -1,5 +1,38 @@
 package object;
 
+import object.base.ApplicationType;
+import object.base.MaterialType;
+
 public class Weapon {
+	private MaterialType material;
+	private ApplicationType application;
+	private int lifetime;
+
+	public Weapon(MaterialType material, ApplicationType application) {
+		this.material = material;
+		this.application = application;
+		setLifetime(material);
+	}
+
+	public int getLifetime() {
+		return lifetime;
+	}
+
+	public MaterialType getMaterial() {
+		return material;
+	}
+
+	public ApplicationType getApplication() {
+		return application;
+	}
+
+	private void setLifetime(MaterialType material) {
+		if (material == MaterialType.WOOD)
+			; // this.lifetime = ;
+		else if (material == MaterialType.STONE)
+			; // this.lifetime = ;
+		else
+			; // this.lifetime = ;
+	}
 
 }
