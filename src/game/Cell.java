f45ca577f47d083ad23009b45777a43ed05a780b
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-11-15 17:41:24 +0700

    Create class Cell

--- src/game/Cell.java
+++ src/game/Cell.java
@@ -1,5 +1,52 @@
 package game;
 
-public class Cell {
+import game.base.Coordinate;
 
+public class Cell {
+	private Coordinate coCell;
+	private boolean isSea;
+	private boolean closed;
+	private boolean status;
+	
+	public Cell(Coordinate coCell, boolean isSea, boolean closed, boolean status) {
+		this.setCoCell(coCell);
+		this.setSea(isSea);
+		this.setClosed(closed);
+		this.setStatus(status);
+	}
+
+	public Coordinate getCoCell() {
+		return coCell;
+	}
+
+	public void setCoCell(Coordinate coCell) {
+		this.coCell = coCell;
+	}
+
+	public boolean isSea() {
+		return isSea;
+	}
+
+	public boolean getStatus() {
+		return status;
+	}
+
+	public void setStatus(boolean status) {
+		this.status = status;
+	}
+
+	public boolean isClosed() {
+		return closed;
+	}
+	
+	private void setSea(boolean isSea) {
+		this.isSea = isSea;
+	}
+
+	private void setClosed(boolean closed) {
+		this.closed = closed;
+	}
+
+	
+	
 }
