ca34829a2f45f9012dee0de17042decc4918af57
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-11-16 16:24:35 +0700

    Create class Metal

--- src/object/Metal.java
+++ src/object/Metal.java
@@ -1,5 +1,32 @@
 package object;
 
-public class Metal {
+import game.Cell;
+import game.Player;
+import game.base.Removeable;
+import object.base.ApplicationType;
+import object.base.MaterialType;
+
+public class Metal implements Removeable {
+	private boolean metal;
+	private Cell position;
+	private int time;
+
+	
+	public Metal(Cell position) {
+		this.metal = false;
+		this.position = position;
+		//this.time =; 
+	}
+	@Override
+	public void beRemoved() {
+		if (Player.getCurrentPosition().getCoCell().isNextTo(this.position.getCoCell())&&metal&&(Player.getCurrentShovel().getMaterial()==MaterialType.METAL)) {
+			Player.setMetal(Player.getMetal()+1);
+			//time =;
+			Player.decrease
+		}
+		// TODO Auto-generated method stub
+		
+		
+	}
 
 }
