ef477f02deff5ee40320d41aec894c0921b3010b
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-11-18 17:23:06 +0700

    Edit class Scrape

--- src/object/Scrape.java
+++ src/object/Scrape.java
@@ -8,50 +8,31 @@ import game.Player;
 import game.base.Removeable;
 import object.base.ApplicationType;
 
-public class Scrape implements Removeable {
-	private boolean scrape;
-	private Cell position;
-	private int time;
+public class Scrape extends Material implements Removeable {
 
 	public Scrape(Cell position) {
-		scrape = true;
-		this.position = position;
+		super(position);
 	}
 
 	@Override
 	public void beRemoved() {
 		if (Player.getCurrentShovel() == null)
 			return;
-		if (Player.getCurrentPosition().isNextTo(position) && scrape) {
-			Player.setScrape(Player.getScrape() + 1);
+		if (Player.getCurrentPosition().isNextTo(position) && present) {
+			Player.setScrape(Player.getScrape()+1);
+			present = false;
 			position.setStatus(true);
 			Player.decreaseLifetime(ApplicationType.SHOVEL);
 			Player.decreaseHP();
 		}
-		for (Scrape x : Map.getAvailableScrape()) {
-			if (x.isScrape())
-				return;
+	}
+
+	public void refresh(Cell position) {
+		if (!present) {
+			this.setPresent(true);
+			this.setPosition(position);
+			this.position.setStatus(false);
 		}
-		// time = ;
-		// TODO Auto-generated method stub
-
-	}
-
-//	public void refresh() {
-//		for (Scrape x : Map.getAvailableScrape()) {
-//			if (x.isScrape())
-//				return;
-//		}
-//		// CheckTime
-//
-//	}
-
-	public boolean isScrape() {
-		return scrape;
-	}
-
-	public Cell getPosition() {
-		return position;
 	}
 
 }
