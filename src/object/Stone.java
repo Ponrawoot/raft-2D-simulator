268a7b9d7903caabf1c960d2aee40ade2e61f0cf
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-11-18 17:21:23 +0700

    Edit class Stone

--- src/object/Stone.java
+++ src/object/Stone.java
@@ -5,42 +5,29 @@ import game.Map;
 import game.Player;
 import game.base.Removeable;
 
-public class Stone implements Removeable{
-	private boolean stone;
-	private Cell position;
-	
+public class Stone extends Material implements Removeable{
+
 	public Stone(Cell position) {
-		this.stone = true;
-		this.position = position;
+		super(position);
 	}
 
 	@Override
 	public void beRemoved() {
 		// TODO Auto-generated method stub
-		if (Player.getCurrentPosition().isNextTo(position)) {
+		if (Player.getCurrentPosition().isNextTo(position)&&present) {
 			Player.setStone(Player.getStone()+1);
+			present = false;
 			position.setStatus(true);
 			Player.decreaseHP();
 		}
-		
-		for (Stone x : Map.getAvailableStone()) {
-			if (x.isStone())
-				return;
-			// time = ;
-			// TODO Auto-generated method stub
-
+	}
+	
+	public void refresh(Cell position) {
+		if (!present) {
+			this.setPresent(true);
+			this.setPosition(position);
+			this.position.setStatus(false);
 		}
 	}
-//		public void refresh() {
-
-	public boolean isStone() {
-		return stone;
-	}
-
-	public Cell getPosition() {
-		return position;
-	}
-	
-	
 
 }
