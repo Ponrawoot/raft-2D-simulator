088d2ab907b9c493643342e52262cbc881cd93c3
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2021-11-18 17:29:50 +0700

    Add timer

--- src/object/Eagle.java
+++ src/object/Eagle.java
@@ -1,5 +1,8 @@
 package object;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import game.Cell;
 import game.Map;
 import game.Player;
@@ -60,6 +63,21 @@ public class Eagle extends Animal {
 			Player.setFeather(Player.getFeather() + 1);
 			Player.decreaseLifetime(ApplicationType.SPEAR);
 			Player.decreaseHP();
+
+			Timer timer = new Timer();
+			timer.scheduleAtFixedRate(new TimerTask() {
+				int i = 15; // (second) can change
+
+				@Override
+				public void run() {
+					// TODO Auto-generated method stub
+					i--;
+					if (i < 0) {
+						timer.cancel();
+						refresh();
+					}
+				}
+			}, 0, 1000);
 		}
 	}
 
