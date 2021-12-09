d6da9dec000df90535abdfe6484f33bdd750b5d9
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-12-09 16:15:12 +0700

    Edit class MarketButton

--- src/component/base/MarketButton.java
+++ src/component/base/MarketButton.java
@@ -3,10 +3,16 @@ package component.base;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class MarketButton extends Button{
 	public MarketButton() {
-		setText("MarketButton");
+		//setText("MarketButton");
+		ImageView ImageView = new ImageView(new Image("market.jpg"));
+		this.setGraphic(ImageView);
+		ImageView.setFitHeight(60);
+		ImageView.setFitWidth(60);
 		setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			
