b4971aec3681bd462af97688d9c295673ef5dc07
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-12-09 15:18:00 +0700

    Edit Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,10 +1,20 @@
 package application;
 
+import component.RootPane;
+import javafx.application.Application;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+	private static RootPane rootPane;
 	public void start(Stage primaryStage) throws Exception {
+		rootPane = new RootPane();
 		
+		Scene scene = new Scene(rootPane, 512,480);
+		
+		primaryStage.setTitle("Survival Simulator");
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	public static void main(String[] args) {
 		launch(args);
