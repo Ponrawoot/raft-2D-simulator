789938591509467f15a1486e32ef2e61df323964
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2021-12-09 15:23:13 +0700

    Edit Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,9 +1,13 @@
 package application;
 
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	public void start(Stage primaryStage) throws Exception {
+		HBox root = new HBox();
 		
 	}
 	public static void main(String[] args) {
