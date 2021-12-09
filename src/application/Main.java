b5affa327cccd205445af6797ca71cdcda3a4a2b
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-12-09 14:07:54 +0700

    Add package application

---
+++ src/application/Main.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.stage.Stage;
+
+public class Main extends Application {
+	public void start(Stage primaryStage) throws Exception {
+		
+	}
+	public static void main(String[] args) {
+		launch(args);
+	}
+	
+
+}
