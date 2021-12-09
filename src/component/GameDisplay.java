2870c68bb023eb2dfbf79b2a85bed8b1e98b8862
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-12-09 16:16:32 +0700

    Create class GameDisplay

---
+++ src/component/GameDisplay.java
@@ -0,0 +1,10 @@
+package component;
+
+import javafx.scene.layout.GridPane;
+
+public class GameDisplay extends GridPane {
+	public GameDisplay() {
+		this.setPrefHeight(300);
+		this.setPrefWidth(300);
+	}
+}
