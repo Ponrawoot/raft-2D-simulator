395878da1b27cdc2308e2dc5427eb00cbe42821a
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-12-09 16:14:31 +0700

    Edit class InventoryButton

--- src/component/base/InventoryButton.java
+++ src/component/base/InventoryButton.java
@@ -3,10 +3,16 @@ package component.base;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 
 public class InventoryButton extends Button {
 	public InventoryButton() {
-		setText("InventoryButton");
+		//setText("InventoryButton");
+		ImageView ImageView = new ImageView(new Image("CategoryOther.png"));
+		this.setGraphic(ImageView);
+		ImageView.setFitHeight(60);
+		ImageView.setFitWidth(60);
 		setOnAction(new EventHandler<ActionEvent>() {
 			@Override
 			
