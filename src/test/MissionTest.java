0f2ef711ecca9327df9df0f9ce9800b5fdee5c0a
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-12-10 11:14:46 +0700

    Add test package

---
+++ src/test/MissionTest.java
@@ -0,0 +1,40 @@
+package test;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import game.Mission;
+import game.Player;
+
+public class MissionTest {
+	private Player player;
+	
+	private Mission mission; 
+	
+	@BeforeEach
+	public void setUpBeforeEachTest()  {
+		player = new Player("Survival boy");
+		mission = new Mission();
+	}
+	
+	@Test
+	public void testMission1() {
+		Player.setWood(20);
+		assertEquals(player.getWood(), 20);
+		assertEquals(player.getStone(), 0);
+		assertEquals(player.getRope(), 0);
+		mission.getReward(0);
+		assertEquals(player.getWood(), 0);
+		assertEquals(player.getStone(), 10);
+		assertEquals(player.getRope(), 5);
+		player.setWood(20);
+		mission.getReward(0);
+		assertTrue(player.getWood()==20);
+		player.setWood(100);
+		mission.getReward(0);
+		assertEquals(player.getWood(), 70);
+		
+	}
+
+}
