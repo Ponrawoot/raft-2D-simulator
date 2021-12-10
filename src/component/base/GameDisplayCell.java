e81569daacefc3cb5a581ae39892ddd31d40dcef
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-12-10 18:43:39 +0700

    Edit class GameDisplayCell

--- src/component/base/GameDisplayCell.java
+++ src/component/base/GameDisplayCell.java
@@ -1,7 +1,27 @@
 package component.base;
 
-import javafx.scene.control.Cell;
+import game.Map;
+import game.Cell;
+import game.base.Coordinate;
+import javafx.geometry.Insets;
 
-public class GameDisplayCell extends Cell {
-	private Cell cell;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+public class GameDisplayCell extends Pane {
+	private int x,y;
+	private Map map;
+	public GameDisplayCell(int x,int y) {
+		this.x = x;
+		this.y = y;
+		map = new Map();
+		Coordinate coCell = new Coordinate(x,y);
+		Cell cell = Map.getCellFromCoordinate(coCell);
+		this.setPrefHeight(15);
+		this.setPrefWidth(15);
+		this.setBackground(new Background(new BackgroundFill(Color.MOCCASIN, CornerRadii.EMPTY, Insets.EMPTY)));
+	}
 }
