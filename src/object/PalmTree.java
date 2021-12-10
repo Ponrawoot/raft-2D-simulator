aff8571ba8e86a8dbd2ccc086146bf98285e729b
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2021-12-10 18:15:06 +0700

    Test and edit PalmTree

--- src/object/PalmTree.java
+++ src/object/PalmTree.java
@@ -1,8 +1,5 @@
 package object;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import game.Cell;
 import game.Player;
 import object.base.ApplicationType;
@@ -37,20 +34,14 @@ public class PalmTree extends Tree {
 			super.setWood(3);
 			super.setLeaf(3);
 
-			Timer timer = new Timer();
-			timer.scheduleAtFixedRate(new TimerTask() {
-				int i = 15; // (second) can change
+			try {
+				Thread.sleep(15000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 
-				@Override
-				public void run() {
-					// TODO Auto-generated method stub
-					i--;
-					if (i < 0) {
-						timer.cancel();
-						setReadyToCut(true);
-					}
-				}
-			}, 0, 1000);
+			setReadyToCut(true);
 		}
 	}
 }
