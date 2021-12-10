56b82088ffe4f83d4a46b8e5bf2878dd4d6f4417
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-12-10 18:46:15 +0700

    Edit class RootPane

--- src/component/RootPane.java
+++ src/component/RootPane.java
@@ -4,17 +4,17 @@ import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
 
 public class RootPane extends VBox {
-	private static TopBar topBar;
-//	private static GameDisplay gameDisplay;
+	//private static TopBar topBar;
+	private static GameDisplay gameDisplay;
 	private static BottomBar bottomBar;
 
 	public RootPane() {
 		super();
-//		topBar = new TopBar();
-//		gameDisplay = new GameDisplay();
+		//topBar = new TopBar(null);
+		gameDisplay = new GameDisplay();
 		bottomBar = new BottomBar();
-		this.getChildren().add(topBar);
-//		this.getChildren().add(gameDisplay);
+		//this.getChildren().add(topBar);
+		this.getChildren().add(gameDisplay);
 		bottomBar.setAlignment(Pos.BOTTOM_LEFT);
 		this.getChildren().add(bottomBar);
 	}
