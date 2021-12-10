e140a9fc5579bd02a3e1294bc364c22a57f3bc49
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-12-10 18:45:24 +0700

    Edit class GameDisplay

--- src/component/GameDisplay.java
+++ src/component/GameDisplay.java
@@ -1,10 +1,27 @@
 package component;
 
+import java.util.ArrayList;
+
+import component.base.GameDisplayCell;
+import javafx.geometry.Insets;
 import javafx.scene.layout.GridPane;
 
 public class GameDisplay extends GridPane {
+	ArrayList<GameDisplayCell> cellList;
 	public GameDisplay() {
+		cellList = new ArrayList<GameDisplayCell>();
+		GameDisplayCell cell;
+		this.setHgap(1);
+		this.setVgap(1);
 		this.setPrefHeight(300);
 		this.setPrefWidth(300);
+		for (int i=0;i<20;i++) {
+			for (int j=0;j<20;j++) {
+				cell = new GameDisplayCell(i, j);
+				this.add(cell,i,j);
+				cellList.add(cell);
+			}
+		}
 	}
 }
+	
