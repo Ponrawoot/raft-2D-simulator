4a01a76fec858c6a296fdc6465d2dda5e158e379
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2021-12-11 21:37:17 +0700

    Fixed TopBar

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,16 +1,18 @@
 package application;
 
 import component.RootPane;
+import game.Player;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	private static RootPane rootPane;
+	private Player player;
 	
 	public void start(Stage primaryStage) throws Exception {
-
-		rootPane = new RootPane();
+		player = new Player("player");
+		rootPane = new RootPane(player);
 		
 		Scene scene = new Scene(rootPane, 512,480);
 		
--- src/component/RootPane.java
+++ src/component/RootPane.java
@@ -1,19 +1,20 @@
 package component;
 
+import game.Player;
 import javafx.geometry.Pos;
 import javafx.scene.layout.VBox;
 
 public class RootPane extends VBox {
-	//private static TopBar topBar;
+	private static TopBar topBar;
 	private static GameDisplay gameDisplay;
 	private static BottomBar bottomBar;
 
-	public RootPane() {
+	public RootPane(Player player) {
 		super();
-		//topBar = new TopBar(null);
+		topBar = new TopBar(player);
 		gameDisplay = new GameDisplay();
 		bottomBar = new BottomBar();
-		//this.getChildren().add(topBar);
+		this.getChildren().add(topBar);
 		this.getChildren().add(gameDisplay);
 		bottomBar.setAlignment(Pos.BOTTOM_LEFT);
 		this.getChildren().add(bottomBar);
