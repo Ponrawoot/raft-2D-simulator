bbcb7838dc7d08eb78b94c0aee3094eaae7c1c25
Nanthicha Makjinda <nanthicha.mak@gmail.com>
2021-12-14 00:08:10 +0700

    Edit Main

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,48 +7,53 @@ import game.Player;
 import game.base.Coordinate;
 import game.base.Direction;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 	private static RootPane rootPane;
 	private Player player;
-	
+
 	public void start(Stage primaryStage) throws Exception {
 		player = new Player("player");
 		rootPane = new RootPane(player);
-		
-		Scene scene = new Scene(rootPane, 1000,1000);
-	
+
+		Scene scene = new Scene(rootPane, 1000, 1000);
+
 		primaryStage.setTitle("Survival Simulator");
 		primaryStage.setScene(scene);
 		primaryStage.show();
-		
+
 		player.setRaft(true);
 		rootPane.setOnKeyPressed((KeyEvent e) -> {
-			String new_code = e.getCode().toString();
+			KeyCode code = e.getCode();
 			int x = player.getCurrentPosition().getCoCell().getX();
 			int y = player.getCurrentPosition().getCoCell().getY();
-			Cell c = Map.getCellFromCoordinate(new Coordinate(x,y));
-			if (new_code.equals("W")) {
+			Cell c = Map.getCellFromCoordinate(new Coordinate(x, y));
+			switch (code) {
+			case W:
 				player.move(Direction.UP);
-			}
-			if (new_code.equals("S")) {
+				break;
+			case S:
 				player.move(Direction.DOWN);
-			}
-			if (new_code.equals("A")) {
+				break;
+			case A:
 				player.move(Direction.LEFT);
-			}
-			if (new_code.equals("D")) {
+				break;
+			case D:
 				player.move(Direction.RIGHT);
+				break;
+			default:
+				System.out.println("Invalid Key.");
+				break;
 			}
 			RootPane.redraw(player, c);
 		});
-	
+
 	}
+
 	public static void main(String[] args) {
 		launch(args);
 	}
