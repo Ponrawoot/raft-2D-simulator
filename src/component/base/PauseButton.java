16b503624e473e4f2c90d381c2f5b7f7b16a2349
Ponrawoot Khamkhanongngam <Minnaja853@gmail.com>
2021-12-14 17:50:16 +0700

    Edit PauseButton

--- src/component/base/PauseButton.java
+++ src/component/base/PauseButton.java
@@ -5,35 +5,35 @@ import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class PauseButton extends Button{
-	private String pauseUrl;
-	private String playUrl;
-	private String status;
+	private boolean pause;
 	
 	public PauseButton() {
-		status = "pause";
+		pause = true;
 		//setText(status); 
-		setImage(pauseUrl);
-		setOnAction(new EventHandler<ActionEvent>() {
+		setImage("pausebutton.png");
+		setOnMouseClicked(new EventHandler<MouseEvent>() {
 			
 			@Override
-			public void handle(ActionEvent arg0) {
+			public void handle(MouseEvent arg0) {
 				// TODO Auto-generated method stub
-				if (status == "pause") {
-					setImage(playUrl);
-					status = "play";
+				if (pause) {
+					setImage("Play.png");
+					pause=false;;
 				}
 				else {
-					setImage(pauseUrl);
-					status = "pause";
+					setImage("pausebutton.png");
+					pause=true;
 				}
 			}
 		});
+		
 	}
 	
 	public void setImage(String url) {
-		ImageView imageView = new ImageView(new Image("pausebutton.png"));// new Image(url)
+		ImageView imageView = new ImageView(new Image(url)); 
 		imageView.setFitHeight(60);
 		imageView.setFitWidth(60);
 		setGraphic(imageView);
